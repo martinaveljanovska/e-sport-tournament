48e00693fbcd746067d74169010ff7eae3fc7abf
Stef Naskova <snaskova094@gmail.com>
2020-05-19 16:08:38 +0200

    chore: move the code in document ready function; removed unnecessary countdown code

--- js/paywall.js
+++ js/paywall.js
@@ -1,13 +1,16 @@
-$(".inplayer-paywall-logout").parent().hide();
-paywall.on("authenticated", function () {
-	$(".inplayer-paywall-login").parent().hide();
-	$(".inplayer-paywall-logout").parent().show();
-});
+$(function () {
+	$(".inplayer-paywall-logout").parent().hide();
+	paywall.on("authenticated", function () {
+		$(".inplayer-paywall-login").parent().hide();
+		$(".inplayer-paywall-logout").parent().show();
+	});
 
-paywall.on("logout", function () {
-	location.reload();
-});
+	paywall.on("logout", function () {
+		location.reload();
+	});
 
-paywall.on("inject", function () {
-	$(".inplayer-paywall").addClass("responsive-iframe");
-});
+	paywall.on("inject", function () {
+		$(".inplayer-paywall").addClass("responsive-iframe");
+	});
+
+})
